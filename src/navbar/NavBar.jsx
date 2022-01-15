be382d92315e022f24c6b0b80378a214cee4d171
JChapman13 <77743972+JChapman13@users.noreply.github.com>
2022-01-15 14:12:06 -0500

    added quick styling to app bar

--- src/navbar/NavBar.jsx
+++ src/navbar/NavBar.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Notifications from "../components/Notifications/Notifications";
+import ProfileDropdown from "../components/ProfileDropdown/ProfileDropdown";
+import Search from "../components/Search/Search";
 
 function NavBar() {
   return <div></div>;
