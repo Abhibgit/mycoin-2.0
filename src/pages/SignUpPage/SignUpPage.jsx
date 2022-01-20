3d4f49c4c56cd1e01e431644101ec016695080e6
aryan ahmed <aryan_ahmed21@aryans-MacBook-Air.local>
2022-01-20 11:53:17 -0500

    signup

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,29 +1,6 @@
 import React from "react";
 import SignUpForm from "../../components/SignUp/SignUp";
 
-<<<<<<< HEAD
-export default class SignUpPage extends React.Component {
-  state = {
-    showLogin: false,
-  };
-
-  render() {
-    return (
-      <main className="SignUpPage">
-        <div>
-          <h3
-            onClick={() => this.setState({ showLogin: !this.state.showLogin })}
-          >
-            {this.state.showLogin ? "SIGN UP" : "LOG IN"}
-          </h3>
-        </div>
-
-        <SignUpForm setUserInState={this.props.setUserInState} />
-      </main>
-    );
-  }
-}
-=======
 function SignupPage(props) {
   const [showLogin, setShowLogin] = React.useState(false);
 
@@ -40,4 +17,3 @@ function SignupPage(props) {
 }
 
 export default SignupPage;
->>>>>>> 62963971b2a3f2a0b3c2f42a47c0e8ffc4680412
