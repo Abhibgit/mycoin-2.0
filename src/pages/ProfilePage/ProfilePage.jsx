73a181f85c06273ee34127c480a681764af11991
Abhishek Bhatia <abhishekbhatia@lin.lan>
2022-01-20 23:33:13 -0500

    profile login, signup, edit, delete, logout together

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import Watchlist from "../../components/Watchlist/Watchlist";
 import Profile from "../../components/Profile/Profile";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function ProfilePage(props) {
-  console.log("Profile Page", props);
+  const { state } = useLocation();
+  console.log("Profile Page", state);
+  let user = "";
+  let setUserInState = "";
+  if (state === null) {
+    user = props.user;
+    setUserInState = props.setUserInState;
+  } else {
+    user = state.user;
+    setUserInState = state.setUserInState;
+  }
+
   return (
     <div>
-      <Profile
-        user={props.user}
-        setUserInState={props.setUserInState}
-      ></Profile>
+      <Profile user={user} setUserInState={setUserInState}></Profile>
     </div>
   );
 }
