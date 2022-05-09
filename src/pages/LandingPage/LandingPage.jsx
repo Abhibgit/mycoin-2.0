8965a4420b0c2a3d33df631f134c3f3483894f29
Abhishek Bhatia <abhishekbhatia@lin.lan>
2022-05-08 21:41:16 -0400

    Adding Landing page, Home page and setup for slideshow

---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function LandingPage() {
+  return <h1>Welcome To Technically Abhi</h1>;
+}
---
+++ src/pages/NewsPage/NewsPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import CoinInformation from "../../components/CoinProfile/CoinInformation";
+import { Grid } from "@mui/material";
+import Watchlist from "../../components/Watchlist/Watchlist";
+import SearchBar from "../../components/SearchBar/SearchBar";
